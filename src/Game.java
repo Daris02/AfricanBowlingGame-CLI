436dd4bd9331344085c069b78654b8e688cd8031
Daris02 <hei.raymond.2@gmail.com>
2024-03-11 14:11:30 +0300

    feat: init game script with cli

---
+++ src/Game.java
@@ -0,0 +1,107 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Game {
+    private static List<List<Integer>> frames = new ArrayList<>();
+    private static List<Integer> allScore = new ArrayList<>();
+    private static Integer scoreActually = 0;
+
+    public static void play(Integer frameNumber, Integer launchNumber) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            for (int i = frameNumber; i > 0; i--) {
+                List<Integer> frame = Arrays.asList(0,0,0);
+                Integer remainingQuilles = 15;
+                
+                scoreActually = view(i, frameNumber, launchNumber, remainingQuilles, scanner, frame, scoreActually);
+                
+                allScore.add(scoreActually);
+
+                if (frames.size() > 0) {
+                    List<Integer> lastFrame = frames.get((5 - frameNumber)-1);
+                    Integer lastScore = allScore.get(allScore.size()-2);
+                    if (lastFrame.contains(15)) {
+                        allScore.set(allScore.size()-2, (lastScore + frame.stream().reduce(0, Integer::sum)));
+                    
+                    } else if (lastFrame.get(0) != 0 && (lastFrame.get(1) == 0 || lastFrame.get(2) == 0)) {
+                        allScore.set(allScore.size()-2, (lastScore + frame.get(0) + frame.get(1)));
+                    
+                    } else if (lastFrame.get(2) != 0 && lastFrame.stream().reduce(0, Integer::sum) == 0) {
+                        allScore.set(allScore.size()-2, (lastScore + frame.get(0) + frame.get(1)));
+                    
+                    }
+                }
+                frames.add(frame);
+                switch (frames.size()) {
+                    case 5:
+                        frameNumber = 0;
+                        i++;
+                        break;
+                    
+                    case 6:
+                        i--;
+                        frameNumber = 0;
+                        break;
+
+                    default:
+                        frameNumber--;
+                        break;
+                }
+            }
+
+            System.out.println("\t1 \t\t2 \t\t3 \t\t4 \t\t5 \t\t ");
+            for (List<Integer> frame : frames) {
+                System.out.print(frame + "\t");
+            }
+            System.out.println("\n\t"+ allScore.get(0) + "\t\t" + allScore.get(1) + "\t\t" + allScore.get(2) + "\t\t" + allScore.get(3) + "\t\t" + allScore.get(4) + "\t\t");
+
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+    private static Integer view(int index, Integer frameNumber, Integer launchNumber, Integer remainingQuilles, Scanner scanner, List<Integer> frame, Integer scoreActually) {
+        for (int j = launchNumber; j > 0; j--) {
+            System.out.println("\nFrame number : F-" + (5 - frameNumber+1));
+            System.out.println("Launch number : L-" + (3 - launchNumber+1));
+            System.out.println("Rest quilles : RQ-" + remainingQuilles);
+            System.out.print("Quilles touch = ");
+            Integer touchQuilles = scanner.nextInt();
+            
+            if (touchQuilles < 0 || touchQuilles > 15) {
+                System.out.println("Quilles touch invalid !!!");
+                System.out.println("Retry with correct value : (Q >= 0) or (Q > 15)");
+                j++;
+                continue;
+
+            } else if (touchQuilles > remainingQuilles) {
+                System.out.println("Quilles touch invalid !!!");
+                System.out.println("Quilles touch must be <= " + remainingQuilles);
+                j++;
+                continue;
+
+            } else {
+                frame.set(3 - launchNumber, touchQuilles);
+                launchNumber--;
+
+                remainingQuilles = remainingQuilles - touchQuilles;
+                if (remainingQuilles == 0 && frame.stream().reduce(0, Integer::sum) == 15) {
+                    scoreActually = scoreActually + 15;
+                } else {
+                    scoreActually = scoreActually + touchQuilles;
+                }
+                
+                System.out.println("Score actually : " + scoreActually);
+                System.out.println("--------------------");
+
+                if (remainingQuilles == 0 || (frameNumber == 1 && j <= 1)) {
+                    index++;
+                    j = 0;
+                    break;
+                }
+            }
+        }
+        return scoreActually;
+    }
+}
